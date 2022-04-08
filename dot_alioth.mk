e11ec340533a66fe7b303ebf92aa4364338965c6
johnmart19 <johnivan19999@gmail.com>
2022-04-08 18:19:33 +0000

    Alioth: Initial DotOS Bringup

--- dot_alioth.mk
+++ dot_alioth.mk
@@ -9,13 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelPlus-UI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 WITH_GAPPS := true
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1440
 
-# PixelPlus-UI Official Stuff
-CUSTOM_BUILD_TYPE := OFFICIAL
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# dotOS Official
+DOT_OFFICIAL := false
 IS_PHONE := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
@@ -26,7 +29,7 @@ PPUI_MAINTAINER := johnmart19
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := dot_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
